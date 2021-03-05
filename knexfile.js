f2901779883e267592c0437ea37e91ad104c9569
Keven Max <kmax136@gmail.com>
2021-03-04 21:08:16 -0300

    refactor: Setup database

---
+++ knexfile.js
@@ -0,0 +1,51 @@
+const path = require("path");
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: path.resolve(__dirname, 'src', 'db', 'migrations'),
+    },
+    seeds: {
+      directory: path.resolve(__dirname, 'src', 'db', 'seeds'),
+    },
+  },
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
