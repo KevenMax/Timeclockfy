3f10ff9e0e0470df01666d59fa6b91435d7020ce
Keven Max <kmax136@gmail.com>
2021-03-06 11:52:46 -0300

    refactor: Change table records

--- src/db/migrations/20210306002715_records.js
+++ src/db/migrations/20210306002715_records.js
@@ -1,13 +1,17 @@
+const { v4: uuidv4 } = require('uuid');
 
 exports.up = function(knex) {
   return knex.schema.createTable('records', function (table) {
-    table.increments();
+    table.uuid('id').primary().defaultTo(uuidv4());
+    table.uuid('user_id').notNullable();
     table.date('date').notNullable();
     table.time('time').notNullable();
     table.json('location').notNullable();
     table.text('picture').notNullable();
     table.boolean('enabled').defaultTo(true);
     table.timestamps(false, true);
+
+    table.foreign('user_id').references('users.id');
   });
 };
 
